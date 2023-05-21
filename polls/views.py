96768693de50cd60b6bdc881781b9de671de8a85
markvrma <markvrma@gmail.com>
2023-05-21 21:19:39 +0530

    added basic views

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import HttpResponse
-
+from .models import Question
 def index(request):
-    return HttpResponse("hello world youre at the polls index")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list]) 
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse("youre lookng at question %s" % question_id)
+
+def results(request, question_id):
+    response = "youre looking at results of question %s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("youre voting on question %s" % question_id)
+
