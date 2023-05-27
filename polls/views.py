24392febaae5e11b1fe248390f78f4bf2fcefc79
markvrma <markvrma@gmail.com>
2023-05-27 23:43:24 +0530

    edited detailView to filter out questions from the future

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,5 @@
 from typing import Any
+from django.db import models
 from django.db.models.query import QuerySet
 from django.shortcuts import get_object_or_404, render
 from django.template import loader
@@ -41,6 +42,12 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        """
+        exludes questions that aren't published yet
+        """
+        return Question.objects.filter(pub_date__lte = timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
