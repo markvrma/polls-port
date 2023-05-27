5b37bae418381d35fd27e6ff9d5dcb079006f3ac
markvrma <markvrma@gmail.com>
2023-05-27 23:31:35 +0530

    fixed bug that publishes future polls

--- polls/views.py
+++ polls/views.py
@@ -5,6 +5,7 @@ from django.template import loader
 from django.http import Http404, HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
@@ -33,7 +34,9 @@ class IndexView(generic.ListView):
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        return Question.objects.order_by("-pub_date")[:5]
+        """Return last five published questions
+        (not incliding those set to be published in the future)"""
+        return Question.objects.filter(pub_date__lte = timezone.now()).order_by("-pub_date")[:5]
     
 class DetailView(generic.DetailView):
     model = Question
